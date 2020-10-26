3e808ea660e17d1cfc310913394987636e135ce5
Prince Kumar <70108171+Singhdev65@users.noreply.github.com>
2020-10-26 23:47:07 +0530

    Add files via upload

---
+++ Learning JavaScript/Lecture/arthematic.js
@@ -0,0 +1,37 @@
+// if modulo of a number is 0 then its a even number orelse its an odd number.
+
+
+/* ----------------challenge---------------- 
+
+var dogAge = prompt("Please Enter your Dog Age: ")
+var humanAge  = ((dogAge -2)* 4) + 21;
+
+alert("Human age as per your Dog's Age is " + humanAge);
+*/
+
+// var modulo = 6 % 4; // 2
+// console.log(modulo);
+
+// modulo operator gives the reminder
+
+
+//-----------------increment/decrement ----------------
+
+/* var x = 5;
+x--;// Decrement
+console.log(x); // 4
+
+
+var x = 5;
+x+=2; // increment by 2
+console.log(x);
+
+
+
+ var x = 4;
+x++; // increment
+console.log(x); // 5
+*/
+
+
+
---
+++ Learning JavaScript/Lecture/function.js
@@ -0,0 +1,8 @@
+// function are used for packeging the codes, i,e if a code is to be used repeatedly then we can used a function to bundle the code.
+
+function evenNumber (a, b){
+    if(a % b == 0){
+        console.log(a + "is a even number")
+    }else 
+    console.log("Odd number")
+}
