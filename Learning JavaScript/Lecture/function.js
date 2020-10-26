44725c80e902affe6301ce5d31edcb1708b390db
Prince Kumar <70108171+Singhdev65@users.noreply.github.com>
2020-10-27 00:36:43 +0530

    Add files via upload

--- Learning JavaScript/Lecture/function.js
+++ Learning JavaScript/Lecture/function.js
@@ -1,8 +1,52 @@
-// function are used for packeging the codes, i,e if a code is to be used repeatedly then we can used a function to bundle the code.
+/* function are used for packeging the codes, i,e if a code is to be used repeatedly then we can used a function to bundle the code.
 
-function evenNumber (a, b){
-    if(a % b == 0){
+function evenNumber (a){
+    if(a % 2 == 0){
         console.log(a + "is a even number")
     }else 
     console.log("Odd number")
+} */
+
+/*
+
+var costPerBottle = 20; 
+    
+
+function getMilk(amount) { 
+    var bottleOfMilk = amount / costPerBottle; 
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    alert("Please give me " + parseInt(bottleOfMilk) + " bottles of milk")
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+
+    return amount % 20;
+  }
+  var change = getMilk(200);
+  console.log(change);
+
+  */
+
+
+//   --------------Challenge ---------------
+
+function bmiCalculator(weight, height) {
+    var BMI = weight / (height * height)
+    return BMI;
 }
+
+bmiCalculator(65, 1.8)
